52f048fbaee0d5f7a894b2593432d3a5cbeb4782
Gaston Mieulet <105996997+Luxcrift@users.noreply.github.com>
2023-02-21 14:40:24 -0300

    Create Telegram.sh

---
+++ .github/workflows/Telegram.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# set -x
+BOT_URL="https://api.telegram.org/bot6124937865:AAEcUK3JnB_YW65I2TGUl1flSddvAMHP058/sendMessage"
+TELEGRAM_CHAT_ID="-1692690779"
+# Set formatting
+PARSE_MODE="Markdown"
+COMMIT=$(git log -1 --pretty=format:"%s")
+BUILD_STATUS="SUCCESS"
+VERSION1=`jq -r '.version' ./frontend/package.json`
+VERSION2=`jq -r '.version' ./products/package.json`
+VERSION3=`jq -r '.version' ./shopping-cart/package.json`
+
+# Send message function
+send_msg () {
+    curl -s -X POST ${BOT_URL} -d chat_id=$TELEGRAM_CHAT_ID \
+        -d text="$1" -d parse_mode=${PARSE_MODE}
+}
+
+# Call send message with the message
+send_msg "
+\`-----------------------------------------------------------\`
+Deploy 🚀*${BUILD_STATUS}!*
+\`Repository 📦:  ${REPOSITORY}\`
+\`Branch 🏷:      ${GITHUB_HEAD_REF}\`
+\`Version Frontend ✅:           ${VERSION1}\`
+\`Version Products ✅:           ${VERSION2}\`
+\`Version Shopping-Cart ✅:      ${VERSION3}\`
+\`Autor ✅:      ${GITHUB_ACTOR}\`
+*Commit Msg 💭:*
+_${COMMIT}_
+[Job Log view here 🤟]("hola")
+\`-----------------------------------------------------------\`
+"
