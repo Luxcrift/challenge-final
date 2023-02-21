a4154567b4fbca6e7c9ab3aed0fc2c33accc53b2
Gaston Mieulet <105996997+Luxcrift@users.noreply.github.com>
2023-02-21 14:58:04 -0300

    Update ec2.tf

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -9,28 +9,7 @@ resource "aws_instance" "develop" {
       "Infra" = "Terraform"
       "Owner" = "DevOps Team"
     }
-    user_data = <<EOF
-#!/bin/bash
-DOMAIN=$(curl icanhazip.com)
-SSLIP="$DOMAIN.sslip.io"
-sudo yum update -y
-sudo yum install -y wget zip unzip docker git jq
-sudo amazon-linux-extras install nginx1 java-openjdk11 -y 
-sudo amazon-linux-extras install epel -y
-sudo systemctl enable nginx
-sudo systemctl start nginx
-sudo service docker start
-sudo usermod -a -G docker ec2-user 
-DC_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/docker/compose/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-sudo curl -L "https://github.com/docker/compose/releases/download/$DC_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo service docker restart
-sudo yum install -y certbot python2-certbot-nginx
-sudo certbot --nginx --register-unsafely-without-email --agree-tos -d $SSLIP --cert-name nginx
-sudo usermod -aG docker nginx
-sudo service docker restart
-EOF
-}
+    user_data = file("https.sh")
 
 resource "aws_instance" "testing" {
     ami = "${var.ami}"
@@ -43,27 +22,7 @@ resource "aws_instance" "testing" {
       "Infra" = "Terraform"
       "Owner" = "DevOps Team"
     }
-    user_data = <<EOF
-#!/bin/bash
-DOMAIN=$(curl icanhazip.com)
-SSLIP="$DOMAIN.sslip.io"
-sudo yum update -y
-sudo yum install -y wget zip unzip docker git jq
-sudo amazon-linux-extras install nginx1 java-openjdk11 -y 
-sudo amazon-linux-extras install epel -y
-sudo systemctl enable nginx
-sudo systemctl start nginx
-sudo service docker start
-sudo usermod -a -G docker ec2-user 
-DC_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/docker/compose/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-sudo curl -L "https://github.com/docker/compose/releases/download/$DC_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo service docker restart
-sudo yum install -y certbot python2-certbot-nginx
-sudo certbot --nginx --register-unsafely-without-email --agree-tos -d $SSLIP --cert-name nginx
-sudo usermod -aG docker nginx
-sudo service docker restart
-EOF
+    user_data = file("https.sh")
 }
 
 resource "aws_instance" "prod" {
@@ -77,25 +36,5 @@ resource "aws_instance" "prod" {
       "Infra" = "Terraform"
       "Owner" = "DevOps Team"
     }
-    user_data = <<EOF
-#!/bin/bash
-DOMAIN=$(curl icanhazip.com)
-SSLIP="$DOMAIN.sslip.io"
-sudo yum update -y
-sudo yum install -y wget zip unzip docker git jq
-sudo amazon-linux-extras install nginx1 java-openjdk11 -y 
-sudo amazon-linux-extras install epel -y
-sudo systemctl enable nginx
-sudo systemctl start nginx
-sudo service docker start
-sudo usermod -a -G docker ec2-user 
-DC_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/docker/compose/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-sudo curl -L "https://github.com/docker/compose/releases/download/$DC_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo service docker restart
-sudo yum install -y certbot python2-certbot-nginx
-sudo certbot --nginx --register-unsafely-without-email --agree-tos -d ${SSLIP} --cert-name nginx
-sudo usermod -aG docker nginx
-sudo service docker restart
-EOF
+    user_data = file("https.sh")
 }
