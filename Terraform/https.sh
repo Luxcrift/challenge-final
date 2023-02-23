42f26301226f86fd70fd06cd2c8698830ebfb1e9
Gaston Mieulet <105996997+Luxcrift@users.noreply.github.com>
2023-02-23 11:19:26 -0300

    Update https.sh

--- Terraform/https.sh
+++ Terraform/https.sh
@@ -13,7 +13,26 @@ DC_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/docker/
 sudo curl -L "https://github.com/docker/compose/releases/download/$DC_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo service docker restart
+cat > cert <<EOF
+server {
+        listen 80;
+        listen [::]:80;
+        root /usr/share/nginx/html;
+        server_name $SSLIP www.$SSLIP;
+        location / {
+                #try_files $uri $uri/ =404;
+                proxy_set_header Host \$host;
+                proxy_set_header X-Real-IP \$remote_addr;
+                proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+                proxy_set_header X-Forwarded-Proto \$scheme;
+                proxy_read_timeout  90s;
+        }
+}
+EOF
+sudo mv cert /etc/nginx/conf.d/cert.conf 
+sudo systemctl restart nginx
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 sudo yum install -y certbot python2-certbot-nginx
-sudo certbot --nginx --register-unsafely-without-email --agree-tos -d "${SSLIP}" --cert-name nginx
-sudo usermod -aG docker nginx
-sudo service docker restart
+sudo certbot --nginx --register-unsafely-without-email --agree-tos -d "${SSLIP}" --cert-name cert
