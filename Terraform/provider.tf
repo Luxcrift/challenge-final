650e4c4d9b2becd11270732cbdde1cad5314a07b
Gaston Mieulet <105996997+Luxcrift@users.noreply.github.com>
2023-03-22 17:20:30 -0300

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "challenge-diosito-123"
+    bucket = "github-gfm"
     key = "global/mystatefile/terraform.tfstate"
     region = "us-east-1"
 
